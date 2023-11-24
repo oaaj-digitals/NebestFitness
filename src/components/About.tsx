8f63c9dff0ed5478f936d08b3c6ae621f8794289
Akintade Japhet <oaaj.digitals@gmail.com>
2023-11-24 23:56:32 +0100

    Update About section
        Replace the video with an image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,6 @@
 import { styled as s } from "styled-components";
 import Section from "./Section";
-import aboutVideo from "../assets/videos/video-1.mp4";
-import playIcon from "../assets/icons/play_circle_FILL0_wght400_GRAD0_opsz24.svg";
-import pauseIcon from "../assets/icons/pause_circle_FILL0_wght400_GRAD0_opsz24.svg";
-import { useEffect, useState } from "react";
+import aboutImage from "../assets/images/IMG_1421.jpg";
 
 const AboutBg = s.div`
     width: 100%;
@@ -26,60 +23,23 @@ const AboutBg = s.div`
 	}
 `;
 
-const VideoBox = s.div`
+const ImageBox = s.div`
     height: 80%;
+	width: 30rem;
     transform: skew(-10deg);
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     overflow: hidden;
-    cursor: pointer;
-    transition: all .4s linear;
     user-select: none;
     user-drag: none;
     -webkit-user-drag: none;
 
-    &:hover .VideoCover {
-        display: inline-block !important;
+	& > img {
+        width: 136%;
+    	transform: skew(10deg);
     }
 
-    & > video {
-        height: 100%;   
-        transform: skew(10deg) scale(1.3);
-    }
-
-    &.isPlaying {
-        transform: skew(0) !important;
-
-        & > video {
-            transform: skew(0) scale(1) !important;
-        }
-
-         & > .VideoCover {
-            transform: translate(-50%, -50%) skew(0)!important;
-        }
-    }
-`;
-
-const VideoCover = s.div`
-    width: 200%;
-    height: 200%;
-
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%) skew(10deg);
-    transition: all .4s linear;
-    background-color: rgb(0,0,0,0.6);
-    
-    & > img {
-        display: inline-block;
-        height: 6rem; 
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
 `;
 
 const TextContentBox = s.div`
@@ -106,60 +66,14 @@ const Text = s.div`
 `;
 
 const About = () => {
-	const [isPlaying, setIsPlaying] = useState(false);
-
-	const videoPlayer = document.getElementById(
-		"about-video"
-	) as HTMLVideoElement;
-
-	const handlePlaying = () => {
-		const videoPlayer = document.getElementById(
-			"about-video"
-		) as HTMLVideoElement;
-		videoPlayer.volume = 0.1;
-		setIsPlaying(!isPlaying);
-	};
-
-	const handleVideoEnd = () => {
-		videoPlayer.currentTime = 0;
-		handlePlaying();
-	};
-
-	useEffect(() => {
-		setIsPlaying(false);
-		videoPlayer ? (videoPlayer.volume = 0.1) : null;
-	}, []);
-
-	useEffect(() => {
-		if (videoPlayer) {
-			isPlaying ? videoPlayer.play() : videoPlayer.pause();
-		}
-	}, [isPlaying]);
-
 	return (
 		<Section id="about-us" padding="0">
 			<AboutBg>
 				<div></div>
 			</AboutBg>
-			<VideoBox className={isPlaying ? "isPlaying" : ""}>
-				<video
-					id="about-video"
-					src={aboutVideo}
-					// poster={videoThumb}
-					onEnded={handleVideoEnd}
-				/>
-				<VideoCover
-					className="VideoCover"
-					style={
-						isPlaying
-							? { display: "none" }
-							: { display: "inline-block" }
-					}
-					onClick={handlePlaying}
-				>
-					<img src={isPlaying ? pauseIcon : playIcon} alt="" />
-				</VideoCover>
-			</VideoBox>
+			<ImageBox>
+				<img src={aboutImage} alt="" />
+			</ImageBox>
 			<TextContentBox>
 				<TitleBox>
 					<Title>About Us</Title>
