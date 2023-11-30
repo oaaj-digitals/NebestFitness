28f64ff650dccb96a6cc9b910abb5822b50cf0d9
Akintade Japhet <oaaj.digitals@gmail.com>
2023-11-30 18:27:25 +0100

    Make Gallery section responsive
        - Change a few content and re-arrange it
        - Make it responsive for mobile and portrait tab views

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,21 +4,23 @@ import gItem1 from "../assets/videos/GVTS-1.mp4";
 import gItem2 from "../assets/images/Image-1.png";
 import gItem3 from "../assets/images/IMG_1421.jpg";
 import gItem4 from "../assets/videos/GVTS-2.mp4";
-import gItem5 from "../assets/images/IMG_1058.jpg";
+import gItem5 from "../assets/videos/video-1.mp4";
 import gItem6 from "../assets/videos/GVTS-3.mp4";
 import gItem7 from "../assets/images/Image-2.png";
 import gItem8 from "../assets/videos/GVTS-5.mp4";
+import { breakpoints } from "../services/design-breakpoints";
 
 const GalleryBox = s.div`
-    width: 100%;
-    height: calc(100vw/3);
+    width: 90%;
+    height: calc(100vw/3.2);
     max-height: 100%;
+    margin-top: 2rem;
 
     display: grid;
     justify-content: center;
     align-items: center;
     align-content: center;
-    grid-template-columns: repeat(12, 1fr);
+    grid-template-columns: repeat(11, 1fr);
     grid-template-rows: repeat(8, 1fr);
 
     & div.grid-item {
@@ -39,51 +41,99 @@ const GalleryBox = s.div`
     & video {
         cursor: pointer;
     }
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        width: 100%;
+        height: max-content;
+        max-height: max-content;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(26, calc(100vw/8));
+	}
 `;
 
 const Item1 = s.div`
-    grid-column: 1/4;
-    grid-row: 1/9;
+    grid-column: 5 / span 3;
+    grid-row: 1 / span 8;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 1 / span 4;
+        grid-row: 9 / span 9;
+    }
 `;
 
 const Item2 = s.div`
-    grid-column: 4/6;
-    grid-row: 1/4;
+    grid-column: 3 / span 2;
+    grid-row: 1 / span 3;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 3 / span 2;
+        grid-row: 1 / span 3;
+    }
 `;
 
 const Item3 = s.div`
-    grid-column: 4/6;
-    grid-row: 4/9;
+    grid-column: 10 / span 2;
+    grid-row: 1 / span 5;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 3 / span 2;
+        grid-row: 18 / span 5;
+    }
 `;
 
 const Item4 = s.div`
-    grid-column: 6/9;
-    grid-row: 1/5;
+    grid-column: 1 / span 2;
+    grid-row: 5 / span 4;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 1 / span 2;
+        grid-row: 5 / span 4;
+    }
 `;
 
 const Item5 = s.div`
-    grid-column: 6/9;
-    grid-row: 5/9;
+    grid-column: 8 / span 2;
+    grid-row: 1 / span 5;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 1 / span 2;
+        grid-row: 18 / span 5;
+    }
 `;
 
 const Item6 = s.div`
-    grid-column: 9/11;
-    grid-row: 1/6;
+    grid-column: 3 / span 2;
+    grid-row: 4 / span 5;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 3 / span 2;
+        grid-row: 4 / span 5;
+    }
 `;
 
 const Item7 = s.div`
-    grid-column: 11/13;
-    grid-row: 1/6;
+    grid-column: 1 / span 2;
+    grid-row: 1 / span 4;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 1 / span 2;
+        grid-row: 1 / span 4;
+    }
 `;
 
 const Item8 = s.div`
-    grid-column: 9/13;
-    grid-row: 6/9;
+    grid-column: 8 / span 4;
+    grid-row: 6 / span 3;
+
+    @media (max-width: ${breakpoints.tabPort}) {
+        grid-column: 1 / span 4;
+        grid-row: 23 / span 4;
+    }
 `;
 
 const Gallery = () => {
 	return (
-		<Section id="gallery">
+		<Section id="gallery" title="Workout Moments">
 			<GalleryBox>
 				<Item1 className="grid-item">
 					<video src={gItem1} controls />
@@ -102,7 +152,7 @@ const Gallery = () => {
 				</Item4>
 
 				<Item5 className="grid-item">
-					<img src={gItem5} alt="" />
+					<video src={gItem5} controls />
 				</Item5>
 
 				<Item6 className="grid-item">
