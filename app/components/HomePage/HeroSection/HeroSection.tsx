a1c4fcb6224ff31d84600c0118ab35264b5ed7ff
Japhet Akintade <oaaj.digitals@gmail.com>
2024-04-10 12:37:56 +0100

    Add social links to the hero section

--- app/components/HomePage/HeroSection/HeroSection.tsx
+++ app/components/HomePage/HeroSection/HeroSection.tsx
@@ -1,5 +1,8 @@
+import { faInstagram, faWhatsapp, faXTwitter, faTiktok, faFacebookF, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Btn from "../../Btn/Btn";
 import styles from "./HeroSection.module.css";
+import Link from "next/link";
 
 const headerBtnStyle = {
   width: "max-content",
@@ -24,6 +27,51 @@ const HeroSection = () => {
         >
           Start Today
         </Btn>
+      </div>
+
+      <div className={styles.socials}>
+        <Link
+          href="https://instagram.com/nebestfitness"
+          target="_blank"
+          aria-label="Link to Instagram account"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="xl" />
+        </Link>
+        <Link
+          href="https://wa.me/message/JX6TOLTPPD26J1"
+          target="_blank"
+          aria-label="Link to Whatsapp account"
+        >
+          <FontAwesomeIcon icon={faWhatsapp} size="xl" />
+        </Link>
+        <Link
+          href="https://x.com/nebestfitness"
+          target="_blank"
+          aria-label="Link to X(Twitter) account"
+        >
+          <FontAwesomeIcon icon={faXTwitter} size="xl" />
+        </Link>
+        <Link
+          href="https://tiktok.com/@nebestfitness"
+          target="_blank"
+          aria-label="Link to TikTok account"
+        >
+          <FontAwesomeIcon icon={faTiktok} size="xl" />
+        </Link>
+        <Link
+          href="https://facebook.com/profile.php?id=100078758541400"
+          target="_blank"
+          aria-label="Link to Facebook page"
+        >
+          <FontAwesomeIcon icon={faFacebookF} size="xl" />
+        </Link>
+        <Link
+          href="https://www.youtube.com/@NebestFitness"
+          target="_blank"
+          aria-label="Link to Youtube page"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="xl" />
+        </Link>
       </div>
     </section>
   );
