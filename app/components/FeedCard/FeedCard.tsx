4066989519550b81035071a9c653a155603ce0bf
Japhet Akintade <oaaj.digitals@gmail.com>
2024-04-11 20:41:34 +0100

    Improve FeedCard component
      - Change to NextJs inBuilt Image tag for adding image to the component
      - Add instagram remote pattern to the config file, to allow access to remote images from instagram

--- app/components/FeedCard/FeedCard.tsx
+++ app/components/FeedCard/FeedCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import styles from "./FeedCard.module.css";
 import { Feed } from "../HomePage/Feeds/Feeds";
+import Image from "next/image";
 
 interface Props {
   feed: Feed;
@@ -11,11 +12,13 @@ const FeedCard = ({ feed }: Props) => {
 
   return (
     <Link className={`${styles.card} ${feed ? null : styles.loading}`} href={feed.permalink || feed.media_url} target="_blank" rel="noopener noreferrer">
-      <img
+      <Image
         src={feed.thumbnail_url || feed.media_url}
         alt=""
         loading="lazy"
         className={styles.img}
+        width={1000}
+        height={1800}
       />
       <p className={styles.caption}>
         {feed.caption && caption + "..."}
