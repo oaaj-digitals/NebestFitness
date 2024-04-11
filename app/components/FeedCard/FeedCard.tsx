1367f922bdd7501d7042dc1fb130504d0edee7c4
Japhet Akintade <oaaj.digitals@gmail.com>
2024-04-11 22:00:47 +0100

    Fix Bug
      - Revert back to using normal img tag to include the images

--- app/components/FeedCard/FeedCard.tsx
+++ app/components/FeedCard/FeedCard.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import styles from "./FeedCard.module.css";
 import { Feed } from "../HomePage/Feeds/Feeds";
-import Image from "next/image";
 
 interface Props {
   feed: Feed;
@@ -12,13 +11,11 @@ const FeedCard = ({ feed }: Props) => {
 
   return (
     <Link className={`${styles.card} ${feed ? null : styles.loading}`} href={feed.permalink || feed.media_url} target="_blank" rel="noopener noreferrer">
-      <Image
+      <img
         src={feed.thumbnail_url || feed.media_url}
         alt=""
         loading="lazy"
         className={styles.img}
-        width={720}
-        height={1280}
       />
       <p className={styles.caption}>
         {feed.caption && caption + "..."}
