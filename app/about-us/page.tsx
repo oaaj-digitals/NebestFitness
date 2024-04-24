6b6c2bc2a3a630d0514e45a4a484277cf25e7c81
Japhet Akintade <oaaj.digitals@gmail.com>
2024-04-24 16:58:08 +0100

    Create the About us route

---
+++ app/about-us/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection/AboutSection';
+import MeetMe from '../components/MeetMe/MeetMe';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <AboutSection />
+      <MeetMe />
+    </main>
+  );
+};
+
+export default AboutPage;
