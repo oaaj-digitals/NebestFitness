b8d728feff793feb2f59a081b6e99e24bec02d36
Japhet Akintade <oaaj.digitals@gmail.com>
2024-04-25 13:14:45 +0100

    Finalize the About page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection/AboutSection';
 import MeetMe from '../components/MeetMe/MeetMe';
+import AboutGallery from '../components/AboutGallery/AboutGallery';
+import AboutAdditionals from '../components/AboutAdditionals/AboutAdditionals';
 
 const AboutPage = () => {
   return (
     <main>
       <AboutSection />
       <MeetMe />
+      <AboutGallery />
+      <AboutAdditionals />
     </main>
   );
 };
