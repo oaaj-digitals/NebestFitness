f81548fa05549b0adbe4415691ead3880bfb1a57
Japhet Akintade <oaaj.digitals@gmail.com>
2025-02-16 13:26:00 +0100

    Dynamically render merch and improve styling

--- app/(shop)/shop/page.tsx
+++ app/(shop)/shop/page.tsx
@@ -1,16 +1,24 @@
 import MerchDisplayCard from "@/app/components/MerchDisplayCard/MerchDisplayCard";
 import styles from "./styles.module.css";
+import merchs from "@/app/utilis/merch";
 
 const ShopPage = () => {
   return (
     <main className={styles.main}>
       <section className={styles.section}>
-        <MerchDisplayCard />
-        <MerchDisplayCard />
-        <MerchDisplayCard />
-        <MerchDisplayCard />
-        <MerchDisplayCard />
-        <MerchDisplayCard />
+
+        {
+          merchs.map(
+            (merch, index) =>
+              <MerchDisplayCard
+                displayimg={merch.displayimg}
+                title={merch.title}
+                price={merch.price}
+                gender={merch.gender}
+                key={index}
+              />
+          )
+        }
       </section>
     </main>
   );
