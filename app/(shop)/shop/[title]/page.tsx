4e3e6cc6cfa7b0575e50a151592d537dc402165a
Japhet Akintade <oaaj.digitals@gmail.com>
2025-02-20 01:10:02 +0100

    Implement generateStaticParams() for Merch pages
      - This commit adds the `generateStaticParams()` function to the merch detail page, enabling static generation of detail pages for each merch item.

---
+++ app/(shop)/shop/[title]/page.tsx
@@ -0,0 +1,66 @@
+import merchs, { Merch } from "@/app/utilis/merch";
+import MerchPageContent, { Related } from "@/app/components/Merchpage/MerchPage";
+
+interface Props {
+  params: { title: string; };
+}
+
+export async function generateStaticParams() {
+
+  const merchsTitle = merchs.map((merch) => merch["title"].toLowerCase().replace(/\s/g, "_"));
+
+  return merchsTitle.map((title) => ({ title: title }));
+}
+
+const generateRelated = (currIndex: number, all: Merch[]) => {
+
+  const merchs = all.filter(item => item.title !== all[currIndex].title);
+
+  let related: Related[] = [];
+  let randomNumber: number | undefined = undefined;
+
+
+  if (merchs.length > 0) {
+    for (let i = 1; i <= 2; i++) {
+      const rand = Math.random();
+
+      let newNumber = Math.floor(rand * merchs.length);
+
+
+      while (randomNumber === newNumber) {
+        newNumber = Math.floor(Math.random() * merchs.length);
+      }
+
+      const merchImg = merchs[newNumber].displayimg;
+      const merchtitle = merchs[newNumber].title;
+      const merchLink = merchtitle.toLowerCase().replace(/\s/g, "_");
+
+      related = [...related, { link: merchLink, img: merchImg }];
+
+      randomNumber = newNumber;
+
+    }
+
+    return related;
+  } else {
+    return null;
+  }
+
+};
+
+const MerchDetailPage = ({ params: { title } }: Props) => {
+  const allMerchs: Merch[] = merchs;
+
+  return (
+
+    merchs.map(
+      (merch, index) => (
+        merch.title.toLocaleLowerCase().replace(/\s/g, "_") ==
+          title ?
+          <></> : null
+      )
+    )
+  );
+};
+
+export default MerchDetailPage;
