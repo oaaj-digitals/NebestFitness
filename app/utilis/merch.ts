61046981c3390fbf569a7d84ef8e83d87f101103
Japhet Akintade <oaaj.digitals@gmail.com>
2025-02-20 03:58:33 +0100

    Update merch data and add product images
      This commit
      - replaces the dummy merch data in `merch.ts` with the correct product information.
      - also adds all corresponding product images to the `assets/images/merch` directory.

--- app/utilis/merch.ts
+++ app/utilis/merch.ts
@@ -1,6 +1,19 @@
 import { StaticImageData } from "next/image";
-import premiumHoodie from "../assets/images/Nebestfitness Premium Hoodie.png"
-import shirt1 from "../assets/images/Nebestfitness Shirt 1.png"
+import premiumHoodieFront from "../assets/images/merchs/Nebestfitness_Premium_Hoodie_Front.png";
+import premiumHoodieBack from "../assets/images/merchs/Nebestfitness_Premium_Hoodie_Back.png";
+
+import lightweightShirtFront from "../assets/images/merchs/Nebestfitness_LightWeight_Shirt_Front.png";
+import lightweightShirtBack from "../assets/images/merchs/Nebestfitness_LightWeight_Shirt_Back.png";
+
+import BeTheBestShirtFront from "../assets/images/merchs/NebestFitness_Be_the_best_Shirt_Front.png"
+import BeTheBestShirtBack from "../assets/images/merchs/NebestFitness_Be_the_best_Shirt_Back.png"
+
+import LegacyShirtFront from "../assets/images/merchs/NebestFitness_Legacy_Shirt_Front.png"
+// import LegacyShirtBack from "../assets/images/merchs/"
+
+import FaceCapFront from "../assets/images/merchs/NebestFitness_Face_Cap_Front.png"
+import FaceCapBack from "../assets/images/merchs/NebestFitness_Face_Cap_Back.png"
+import FaceCapSide from "../assets/images/merchs/NebestFitness_Face_Cap_Side.png"
 
 export interface Merch {
   displayimg: string | StaticImageData;
@@ -9,14 +22,14 @@ export interface Merch {
   gender?: string;
   title: string;
   description?:string;
-  availableSizes?:string[];
+  availableSizes?:string | string[];
 }
 
 const merchs : Merch[]=[
   {
     title: "Premium Hoodie",
-    displayimg: premiumHoodie,
-    imgs:[premiumHoodie, shirt1, premiumHoodie],
+    displayimg: premiumHoodieFront,
+    imgs:[premiumHoodieBack],
     price: 45.00,
     gender: "Unisex",
     description:"Perfect for warm-ups, post-workout comfort, or casual wear, our unisex hoodie keeps you cozy while maintaining a sleek alethic look.",
@@ -24,17 +37,35 @@ const merchs : Merch[]=[
   },
   {
     title: "Lightweight Shirt",
-    displayimg: shirt1,
-    imgs:[premiumHoodie, shirt1, premiumHoodie],
+    displayimg: lightweightShirtFront,
+    imgs:[lightweightShirtBack],
+    price: 35.00,
+    gender: "Unisex",
+    description:"Designed for movement, our fitness t-shirt are lightweight, breathable and sweat-wicking, ensuring you stay cool and comfortable during workouts.",
+    availableSizes:["s","m", "l", 'xl'],
+  },
+    {
+    title: "NebestFitness Face Caps",
+    displayimg: FaceCapFront,
+    imgs:[FaceCapSide,FaceCapBack],
+    price: 35.00,
+    gender: "Unisex",
+    description:"A stylish and functional accessory, our face caps provide shade ad a sporty look for any fitness enthusiast.",
+    availableSizes:'One size fits all (adjustable strap)',
+  },
+  {
+    title: "NebestFitness Legacy Shirt",
+    displayimg: LegacyShirtFront,
+    imgs:[lightweightShirtBack],
     price: 35.00,
     gender: "Unisex",
     description:"Designed for movement, our fitness t-shirt are lightweight, breathable and sweat-wicking, ensuring you stay cool and comfortable during workouts.",
     availableSizes:["s","m", "l", 'xl'],
   },
   {
-    title: "NebestFitness Legacy Shirt",
-    displayimg: shirt1,
-    imgs:[premiumHoodie, shirt1, premiumHoodie],
+    title: "NebestFitness Be the Best Shirt",
+    displayimg: BeTheBestShirtFront,
+    imgs:[BeTheBestShirtBack],
     price: 35.00,
     gender: "Unisex",
     description:"Designed for movement, our fitness t-shirt are lightweight, breathable and sweat-wicking, ensuring you stay cool and comfortable during workouts.",
